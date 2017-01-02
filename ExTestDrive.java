704cbbc61222b8170d0191b0f6a5ee49fc708cc2
nomad <nomad@nomad-All-Series>
2017-01-02 15:47:31 +0100

    Exception handling

---
+++ ExTestDrive.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nomad
+ */
+import static java.lang.System.out;
+
+class MyEx extends Exception {
+};
+
+public class ExTestDrive {
+
+    public static void main(String[] args) {
+
+        String test = args[0];
+        out.print("t");
+
+        try {
+            doRisky(test);
+        } catch (MyEx e) {
+            out.print("a");
+        } finally {
+            out.print("w");
+            out.print("s");
+        }
+    }
+
+    private static void doRisky(String t) throws MyEx {
+        out.print("h");
+        if ("yes".equals(t)) {
+            throw new MyEx();
+        }
+        out.print("r");
+        out.print("o");
+    }
+
+}
