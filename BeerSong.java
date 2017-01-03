163ba2989adfa317751046a9e2cd090c1eccd7e2
nomad <nomad@nomad-All-Series>
2017-01-03 21:30:37 +0100

    license update

--- BeerSong.java
+++ BeerSong.java
@@ -1,4 +1,8 @@
-
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 public class BeerSong {
 
     public static void main(String[] args) {
--- DooBee.java
+++ DooBee.java
@@ -1,4 +1,8 @@
-
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 public class DooBee {
 
     public static void main(String[] args) {
