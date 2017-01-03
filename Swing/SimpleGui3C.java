5978762cf32e2ca563e4a4e22af2539cbb9facff
nomad <nomad@nomad-All-Series>
2017-01-03 15:42:51 +0100

    implemented repaint function

--- Swing/SimpleGui3C.java
+++ Swing/SimpleGui3C.java
@@ -22,7 +22,9 @@ public class SimpleGui3C implements ActionListener {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        SimpleGui3C gui = new SimpleGui3C();
+        gui.go();
+        
     }
 
     public void go() {
@@ -38,6 +40,7 @@ public class SimpleGui3C implements ActionListener {
         frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
         frame.setSize(300, 300);
         frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
 
     }
 
@@ -52,10 +55,24 @@ public class SimpleGui3C implements ActionListener {
         
         @Override
         public void paintComponent(Graphics g) {
+            Graphics2D g2d = (Graphics2D) g;
 
+            int red = (int) (Math.random() * 256);
+            int green = (int) (Math.random() * 256);
+            int blue = (int) (Math.random() * 256);
+            Color startColor = new Color(red, green, blue);
+
+            red = (int) (Math.random() * 256);
+            green = (int) (Math.random() * 256);
+            blue = (int) (Math.random() * 256);
+            Color endColor = new Color(red, green, blue);
+            
+            GradientPaint gradient = new GradientPaint(70, 70, startColor, 150, 150, endColor);
+            g2d.setPaint(gradient);
+            g2d.fillOval(70, 70, 100, 100);
         }
     }
-    
+
     
     
 }
