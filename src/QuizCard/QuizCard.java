36a8e548103b2ab225f2bcbd26c89dda88ba3ffd
nomad <>
2017-01-10 17:02:44 +0100

    QuizCard.java: coded as requested in the given UML schema. Note to myself: The word schema is simply a contraction (shortened version) of the phrase schematic diagram

--- src/QuizCard/QuizCard.java
+++ src/QuizCard/QuizCard.java
@@ -11,4 +11,49 @@ package QuizCard;
  */
 public class QuizCard {
     
+    private String question;
+    private String answer;
+
+    public QuizCard(String q, String a) {
+        this.question = q;
+        this.answer = a;
+    }
+    
+    /**
+     * Get the value of answer
+     *
+     * @return the value of answer
+     */
+    public String getAnswer() {
+        return answer;
+    }
+
+    /**
+     * Set the value of answer
+     *
+     * @param answer new value of answer
+     */
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+
+    /**
+     * Get the value of question
+     *
+     * @return the value of question
+     */
+    public String getQuestion() {
+        return question;
+    }
+
+    /**
+     * Set the value of question
+     *
+     * @param question new value of question
+     */
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
 }
