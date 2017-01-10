20921c580c73acd16aabf0ea236eb2de442cef02
nomad <>
2017-01-10 16:09:05 +0100

    aggiunto un commento per mostrare a mia mamma come funziona git :-)

--- src/Swing/BeatBox.java
+++ src/Swing/BeatBox.java
@@ -36,6 +36,8 @@ public class BeatBox {
     int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
 
     public static void main(String[] args) {
+        
+        // aggiungo un commento per mia mamma
         new BeatBox().buildGUI();
     }
 
