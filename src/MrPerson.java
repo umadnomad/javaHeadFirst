c0ceee2971b25d126db43c448457ed6dc6917b5f
nomad <>
2017-01-11 15:31:52 +0100

    MrPerson.java added for practice

---
+++ src/MrPerson.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nomad
+ */
+public class MrPerson {
+    
+    private String name;
+    private byte age;
+    private char sex;
+
+    public MrPerson(String name, byte age, char sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    
+    
+}
